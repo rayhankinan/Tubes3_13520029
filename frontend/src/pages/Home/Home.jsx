f32d668940311def2b4f7cb6be8812485c97f6a9
IloveNooodles <mgarebaldhie80@gmail.com>
2022-04-17 03:31:25 +0700

    finished css for base in index.css

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.red}>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
