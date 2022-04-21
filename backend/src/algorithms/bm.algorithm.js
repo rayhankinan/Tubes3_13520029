2888c2b01e13db5fff098fcdbe204e29ee4f369d
rayhankinan <rayhankinan@gmail.com>
2022-04-21 15:49:23 +0700

    Add algorithms

---
+++ backend/src/algorithms/bm.algorithm.js
@@ -0,0 +1,39 @@
+const buildLast = (pattern) => {
+    const last = new Array(128).fill(-1) // ASCII
+
+    for (let i = 0; i < pattern.length; i++) {
+        last[pattern.charCodeAt(i)] = i;
+    }
+
+    return last
+}
+
+module.exports = (text, pattern) => {
+    const n = text.length
+    const m = pattern.length
+    const last = buildLast(pattern)
+
+    let i = m - 1
+    let j = m - 1
+
+    if (i > n - 1) {
+        return -1
+
+    } else {
+        do {
+            if (pattern.charAt(j) === text.charAt(i)) {
+                if (j === 0) {
+                    return i
+                } else {
+                    i--
+                    j--
+                }
+            } else {
+                const lo = last[text.charCodeAt(i)]
+                i += (m - Math.min(j, 1 + lo))
+                j = m - 1
+            }
+
+        } while (i <= n - 1)
+    }
+}
