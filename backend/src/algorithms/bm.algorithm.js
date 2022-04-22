44f695a7d8aedc406f781a847d8b41f7e47768d3
IloveNooodles <mgarebaldhie80@gmail.com>
2022-04-23 02:32:13 +0700

    change export algo

--- backend/src/algorithms/bm.algorithm.js
+++ backend/src/algorithms/bm.algorithm.js
@@ -1,39 +1,39 @@
+// last found character in the pattern
 const buildLast = (pattern) => {
-    const last = new Array(128).fill(-1) // ASCII
+  const last = new Array(128).fill(-1); // ASCII with 128 chars
 
-    for (let i = 0; i < pattern.length; i++) {
-        last[pattern.charCodeAt(i)] = i;
-    }
+  for (let i = 0; i < pattern.length; i++) {
+    last[pattern.charCodeAt(i)] = i;
+  }
+  return last;
+};
 
-    return last
-}
+const bm = (text, pattern) => {
+  const n = text.length;
+  const m = pattern.length;
+  const last = buildLast(pattern);
 
-module.exports = (text, pattern) => {
-    const n = text.length
-    const m = pattern.length
-    const last = buildLast(pattern)
+  let i = m - 1;
+  let j = m - 1;
 
-    let i = m - 1
-    let j = m - 1
+  if (i > n - 1) {
+    return -1;
+  } else {
+    do {
+      if (pattern.charAt(j) === text.charAt(i)) {
+        if (j === 0) {
+          return i;
+        } else {
+          i--;
+          j--;
+        }
+      } else {
+        const lo = last[text.charCodeAt(i)];
+        i += m - Math.min(j, 1 + lo);
+        j = m - 1;
+      }
+    } while (i <= n - 1);
+  }
+};
 
-    if (i > n - 1) {
-        return -1
-
-    } else {
-        do {
-            if (pattern.charAt(j) === text.charAt(i)) {
-                if (j === 0) {
-                    return i
-                } else {
-                    i--
-                    j--
-                }
-            } else {
-                const lo = last[text.charCodeAt(i)]
-                i += (m - Math.min(j, 1 + lo))
-                j = m - 1
-            }
-
-        } while (i <= n - 1)
-    }
-}
+export { bm };
--- backend/src/algorithms/kmp.algorithm.js
+++ backend/src/algorithms/kmp.algorithm.js
@@ -1,49 +1,50 @@
 const computeFail = (pattern) => {
-    const m = pattern.length
-    const fail = new Array(m)
+  const m = pattern.length;
+  const fail = new Array(m);
 
-    let i = 1
-    let j = 0
+  let i = 1;
+  let j = 0;
 
-    fail[0] = 0
-    while (i < m) {
-        if (pattern.charAt(i) === pattern.charAt(j)) {
-            fail[i] = j + 1
-            i++
-            j++
-        } else if (j > 0) {
-            j = fail[j - 1]
-        } else {
-            fail[i] = 0
-            i++
-        }
+  fail[0] = 0;
+  while (i < m) {
+    if (pattern.charAt(i) === pattern.charAt(j)) {
+      fail[i] = j + 1;
+      i++;
+      j++;
+    } else if (j > 0) {
+      j = fail[j - 1];
+    } else {
+      fail[i] = 0;
+      i++;
     }
+  }
+  return fail;
+};
 
-    return fail
-}
+const kmp = (text, pattern) => {
+  const n = text.length;
+  const m = pattern.length;
+  const fail = computeFail(pattern);
 
-module.exports = (text, pattern) => {
-    const n = text.length
-    const m = pattern.length
-    const fail = computeFail(pattern)
+  let i = 0;
+  let j = 0;
 
-    let i = 0
-    let j = 0
-
-    while (i < n) {
-        if (pattern.charAt(j) === text.charAt(i)) {
-            if (j === m - 1) {
-                return i - m + 1
-            } else {
-                i++
-                j++
-            }
-        } else if (j > 0) {
-            j = fail[j - 1]
-        } else {
-            i++
-        }
+  while (i < n) {
+    if (pattern.charAt(j) === text.charAt(i)) {
+      if (j === m - 1) {
+        return i - m + 1;
+      } else {
+        i++;
+        j++;
+      }
+    } else if (j > 0) {
+      j = fail[j - 1];
+    } else {
+      i++;
     }
+  }
 
-    return -1
-}
+  return -1;
+};
+
+export { kmp };
