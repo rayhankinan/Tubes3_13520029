9c0f38efc79dac1aaf0ce27a717fe6c978081ba6
IloveNooodles <mgarebaldhie80@gmail.com>
2022-04-23 19:03:14 +0700

    regex complete

--- backend/src/algorithms/sanitazion.js
+++ backend/src/algorithms/sanitazion.js
@@ -7,13 +7,15 @@ console.log(dnaMatching("ACGTAAAAAAAAACACACACACACACGT"));
 
 const dateMatching = (input) => {
   // all date ?
-  const allDate = new RegExp(
-    /([12][0-9]|3[01]|0?[1-9])([-\\/\s])(1[0-2]|0?[1-9]|(januari|februari|maret|april|mei|juni|july|agustus|september|oktober|november|desember))\2(\d{4})/,
-    "gi"
+  const allRegex = new RegExp(
+    /^([12][0-9]|3[01]|0?[1-9])([-\\/\s])(1[0-2]|0?[1-9]|(januari|februari|maret|april|mei|juni|july|agustus|september|oktober|november|desember)\2\d{4})(\s([A-Za-z]+[-s]?)+[0-9]{0,2})?$/,
+    "i"
   );
-
-  const day = new RegExp(/[12][0-9]|3[0-1]|0?[0-9]/, "g");
-  const month = new Reg();
+  const allDate = new RegExp(
+    /^([12][0-9]|3[01]|0?[1-9])([-\\/\s])(1[0-2]|0?[1-9]|(januari|februari|maret|april|mei|juni|july|agustus|september|oktober|november|desember)\2\d{4})$/,
+    "i"
+  );
+  const penyakit = new RegExp(/^([A-Za-z]+[-/.\s]?)+[0-9]{0,2}$/, "i");
 };
 
 export { inputSanitation };
