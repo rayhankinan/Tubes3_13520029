f54a28afdbea783b63edbe62b8b33692af2b1086
rayhankinan <rayhankinan@gmail.com>
2022-04-23 19:42:06 +0700

    Fix backend

--- backend/src/algorithms/bm.algorithm.js
+++ backend/src/algorithms/bm.algorithm.js
@@ -1,39 +1,38 @@
-// last found character in the pattern
 const buildLast = (pattern) => {
-  const last = new Array(128).fill(-1); // ASCII with 128 chars
+	const last = new Array(128).fill(-1) // ASCII with 128 chars
 
-  for (let i = 0; i < pattern.length; i++) {
-    last[pattern.charCodeAt(i)] = i;
-  }
-  return last;
-};
+	for (let i = 0; i < pattern.length; i++) {
+		last[pattern.charCodeAt(i)] = i
+	}
 
-const bm = (text, pattern) => {
-  const n = text.length;
-  const m = pattern.length;
-  const last = buildLast(pattern);
+	return last
+}
 
-  let i = m - 1;
-  let j = m - 1;
+module.exports = (text, pattern) => {
+	const n = text.length
+	const m = pattern.length
+	const last = buildLast(pattern)
 
-  if (i > n - 1) {
-    return -1;
-  } else {
-    do {
-      if (pattern.charAt(j) === text.charAt(i)) {
-        if (j === 0) {
-          return i;
-        } else {
-          i--;
-          j--;
-        }
-      } else {
-        const lo = last[text.charCodeAt(i)];
-        i += m - Math.min(j, 1 + lo);
-        j = m - 1;
-      }
-    } while (i <= n - 1);
-  }
-};
+	let i = m - 1
+	let j = m - 1
 
-export { bm };
+	if (i > n - 1) {
+		return -1
+		
+	} else {
+		do {
+			if (pattern.charAt(j) === text.charAt(i)) {
+				if (j === 0) {
+					return i
+				} else {
+					i--
+					j--
+				}
+			} else {
+				const lo = last[text.charCodeAt(i)]
+				i += m - Math.min(j, 1 + lo)
+				j = m - 1
+			}
+		} while (i <= n - 1)
+	}
+}
--- backend/src/algorithms/levenshtein.algorithm.js
+++ backend/src/algorithms/levenshtein.algorithm.js
@@ -1,39 +1,34 @@
 // use memoization (dp) because use same computation for subprolem
-const levenshtein = (text, target) => {
-  const n = text.length;
-  const m = target.length;
-  const dp = new Array(n + 1);
+module.exports = (text, target) => {
+    const n = text.length;
+    const m = target.length;
+    const dp = new Array(n + 1);
 
-  //fill all table with 0 with text x target
-  for (let i = 0; i <= n; i++) {
-    dp[i] = new Array(m + 1).fill(0);
-  }
-
-  for (let i = 0; i <= n; i++) {
-    for (let j = 0; j <= m; j++) {
-      // empty text insert target
-      if (i == 0) {
-        dp[i][j] = j;
-      }
-      // empty target insert text
-      else if (j == 0) {
-        dp[i][j] = i;
-      }
-      //if same no need computation
-      else if (text[i - 1] === target[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1];
-      }
-      // min of insert, delete, replace
-      else {
-        dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
-      }
+    //fill all table with 0 with text x target
+    for (let i = 0; i <= n; i++) {
+        dp[i] = new Array(m + 1).fill(0)
     }
-  }
 
-  return dp[n][m];
-};
+    for (let i = 0; i <= n; i++) {
+        for (let j = 0; j <= m; j++) {
+            // empty text insert target
+            if (i == 0) {
+                dp[i][j] = j
+            }
+            // empty target insert text
+            else if (j == 0) {
+                dp[i][j] = i
+            }
+            //if same no need computation
+            else if (text[i - 1] === target[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            // min of insert, delete, replace
+            else {
+                dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+            }
+        }
+    }
 
-// console.log(levenshtein("kitten", "sitting"));
-// console.log(Math.min(1, 2, 3, 4));
-
-export { levenshtein };
+    return dp[n][m]
+}
